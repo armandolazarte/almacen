479d33d70be34ab9aaafad07c38183bc845e84d8
Samuel Mercado <samuel.mg@gmx.com>
2015-01-02 08:38:52 +0000

    Cambio en configuración de alias para Fpdf

--- app/config/app.php
+++ app/config/app.php
@@ -189,7 +189,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
-		'Fpdf'              => 'Anouar\Fpdf\Facades\Fpdf',
+		'Fpdf'              => 'Anouar\Fpdf\Fpdf',
 
 	),
 
